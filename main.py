1ed04c50ca36c50bb4f782dc77cd947cf1174307
ottosignals <123465193+ottosignals@users.noreply.github.com>
2023-10-10 11:41:04 +0200

    Changed input params

--- main.py
+++ main.py
@@ -19,25 +19,25 @@ def run():
     "dateTo": os.environ.get('date_to'),
     "numOfDays": int(os.environ.get('num_of_days', 0)),
     
-    "numOfDaysUpdated": int(os.environ.get('num_of_days_updated', 0))
+    "numOfDaysUpdated": int(os.environ.get('num_of_days_updated', 3))
   }
 
   filter = {}
-
-  if params["numOfDaysUpdated"] > 0:
-    filter["dateUpdated"] = {}
-    filter["dateUpdated"]["ge"] = datetime.datetime.now() - datetime.timedelta(days=params["numOfDaysUpdated"])
-    filter["dateUpdated"]["ge"] =  filter["dateUpdated"]["ge"].replace(hour=0, minute=0, second=0, microsecond=0).isoformat(sep=' ')
-    filter["dateUpdated"]["le"] = datetime.datetime.now() 
-    filter["dateUpdated"]["le"] =  filter["dateUpdated"]["le"].replace(hour=0, minute=0, second=0, microsecond=0).isoformat(sep=' ')
+  if params["dateFrom"]:
+    filter["dateFrom"] = datetime.datetime.strptime(params["dateFrom"], '%Y-%m-%d').isoformat(sep=' ')
+    filter["dateTo"] = datetime.datetime.strptime(params["dateTo"], '%Y-%m-%d').isoformat(sep=' ') 
   elif params["numOfDays"] > 0:
     filter["dateFrom"] = datetime.datetime.now() - datetime.timedelta(days=params["numOfDays"])
     filter["dateFrom"] = filter["dateFrom"].replace(hour=0, minute=0, second=0, microsecond=0).isoformat(sep=' ')
     filter["dateTo"] = datetime.datetime.now() 
     filter["dateTo"] = filter["dateTo"].replace(hour=0, minute=0, second=0, microsecond=0).isoformat(sep=' ')
-  elif params["dateFrom"]:
-    filter["dateFrom"] = datetime.datetime.strptime(params["dateFrom"], '%Y-%m-%d').isoformat(sep=' ')
-    filter["dateTo"] = datetime.datetime.strptime(params["dateTo"], '%Y-%m-%d').isoformat(sep=' ') 
+  elif params["numOfDaysUpdated"] > 0:
+    filter["dateUpdated"] = {}
+    filter["dateUpdated"]["ge"] = datetime.datetime.now() - datetime.timedelta(days=params["numOfDaysUpdated"])
+    filter["dateUpdated"]["ge"] =  filter["dateUpdated"]["ge"].replace(hour=0, minute=0, second=0, microsecond=0).isoformat(sep=' ')
+    filter["dateUpdated"]["le"] = datetime.datetime.now() 
+    filter["dateUpdated"]["le"] =  filter["dateUpdated"]["le"].replace(hour=0, minute=0, second=0, microsecond=0).isoformat(sep=' ')
+
 
   api = WPJApi(API_DOMAIN, API_KEY)
   bq = BigQueryApi()

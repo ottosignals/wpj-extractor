4bd0d9e441eb7ad8569af4b4be1bcc4da283357b
Otto Hruby <xhruby27@vutbr.cz>
2023-10-13 10:32:29 +0200

    load batches of rows to bigquery

--- bigquery/api.py
+++ bigquery/api.py
@@ -5,7 +5,7 @@ class BigQueryApi():
   def __init__(self):
     self._client = bigquery.Client()
 
-  def insert(self, project, dataset, table, dataframe, time_partitioning=None, schema=None):
+  def insert(self, project, dataset, table, rows, time_partitioning=None, schema=None, batch_size=None):
     dataset_id = bigquery.Dataset(f"{project}.{dataset}")
     dataset_id.location = 'EU'
     self._client.create_dataset(dataset_id, exists_ok=True)
@@ -19,14 +19,18 @@ class BigQueryApi():
     # job_config.ignore_unknown_values = True
 
     table_id = f"{project}.{dataset}.{table}"
-    job = self._client.load_table_from_json(
-      dataframe, table_id, job_config=job_config
-    )
+    
+    # Split the rows into smaller batches
+    for i in range(0, len(rows), batch_size):
+      batch = rows[i:i + batch_size]
+      job = self._client.load_table_from_json(
+        batch, table_id, job_config=job_config
+      )
 
-    try:
-      job.result()
-    except BadRequest as e:
-      for e in job.errors:
-        print(f"BigQuery error: {e['message']}")
-      raise Exception("BigQuery error")
+      try:
+        job.result()
+      except BadRequest as e:
+        for e in job.errors:
+          print(f"BigQuery error: {e['message']}")
+          raise Exception("BigQuery error")
       
